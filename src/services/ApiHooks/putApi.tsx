2a4d28667316fa8b9448718d8c7e8e34efe3dce3
Muneeb Qureshi <muhammad.muneeb@codistan.org>
2024-12-08 17:08:38 +0500

    Fix: Some api issues

--- src/services/ApiHooks/putApi.tsx
+++ src/services/ApiHooks/putApi.tsx
@@ -4,11 +4,10 @@ import { dataServer } from "../axiosConfig";
 const usePutApi = () => {
   const { postCurd } = Endpoints();
   const updatdePostApi = async (data: any) => {
-    console.log(data);
     try {
       const response = await dataServer.put(postCurd.update_post(data.id), {
         title: data.title,
-        body: data.description,
+        body: data.body,
       });
       return response;
     } catch (err: any) {
